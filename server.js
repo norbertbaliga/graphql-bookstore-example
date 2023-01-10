aab66dd78771c461819ecd6165c7d512faacbd15
Norbert Baliga <norbert.baliga@logmein.com>
2023-01-10 16:43:27 +0100

    Move sample data into individual JSON files

--- server.js
+++ server.js
@@ -12,22 +12,8 @@ const {
 const app = express()
 
 /* ============ Test Database with sample data ============ */
-const authors = [
-	{ id: 1, name: 'J. K. Rowling' },
-	{ id: 2, name: 'J. R. R. Tolkien' },
-	{ id: 3, name: 'Brent Weeks' }
-]
-
-const books = [
-	{ id: 1, name: 'Harry Potter and the Chamber of Secrets', authorId: 1 },
-	{ id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1 },
-	{ id: 3, name: 'Harry Potter and the Goblet of Fire', authorId: 1 },
-	{ id: 4, name: 'The Fellowship of the Ring', authorId: 2 },
-	{ id: 5, name: 'The Two Towers', authorId: 2 },
-	{ id: 6, name: 'The Return of the King', authorId: 2 },
-	{ id: 7, name: 'The Way of Shadows', authorId: 3 },
-	{ id: 8, name: 'Beyond the Shadows', authorId: 3 }
-]
+const authors = require('./data/authors.json')
+const books = require('./data/books.json')
 /* ============ End: Test Database with sample data ============ */
 
 const BookType = new GraphQLObjectType({
